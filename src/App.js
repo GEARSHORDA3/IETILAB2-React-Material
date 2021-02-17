ced4a68142058ed5c5d70c5a6a6a644810552ff3
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-02-17 18:26:05 -0500

    parte 2

--- src/App.js
+++ src/App.js
@@ -1,86 +1,62 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import './Components/App.css';
+import logo from './logo.svg';
 import {TodoList} from './Components/TodoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Login} from './Components/Login'
+import {Login} from './Components/Login.js';
+import {TodoApp} from './Components/TodoApp.js';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import CardMedia from '@material-ui/core/CardMedia';
+import {Card, CardContent, makeStyles} from "@material-ui/core";
+import imageUrl from './images/fondo2.jpg'
 
-export class App extends React.Component {
+const LoginView = () => (
+	<Login/>
+);
+
+const TodoAppView = () => (
+	<TodoApp/>
+);
+
+export class App extends Component {
+
 
 	constructor(props) {
-		super(props);
-
-		const todos = [
-			{text: "Learn React", priority: 5, dueDate: new Date()},
-			{text: "Learn about APIs", priority: 4, dueDate: new Date(2020, 1, 23)},
-			{text: "write TODO App", priority: 3, dueDate: new Date(2020, 1, 30)}];
-
-		this.state = {items: todos, text: '', priority: ''};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.renderRow = this.renderRow.bind(this);
-	}
-
-	renderRow(array) {
-		return array.map((item, i) =>
-			<tr key={i}>
-				<td>{item.text}</td>
-				<td>{item.priority}</td>
-				<td>{item.dueDate.toString()}</td>
-			</tr>
-		);
-	}
-
-	handleSubmit(event) {
-		event.preventDefault();
-		if (!this.state.text.length || this.state.priority == '') {
-			document.getElementById("form").reset();
-			return;
-		}
-		const newItem = {text: this.state.text, priority: this.state.priority, dueDate: new Date()};
-		this.setState(prevState => ({
-			items: prevState.items.concat(newItem)
-		}));
-		document.getElementById("form").reset();
-	}
-
-	handleChange(event) {
-		const key = event.target.name;
-		const value = event.target.value;
-		this.setState({[key]: value});
+		super(props)
+		this.state = {isLoggedLn : false}
+		this.handleLogChange = this.handleLogChange.bind(this);
 	}
 
 
 	render() {
 		return (
-			<div>
-				<h3>TODO</h3>
-				<label>
-					<form id="form" onSubmit={this.handleSubmit}>
-						<label id="activity">Activity :</label>
-						<input type="text" name="text" onChange={this.handleChange}/>
-						<label id="Priority">Priority :</label>
-						<input type="text" name="priority" onChange={this.handleChange}/>
-						<button style={{marginTop: "40px"}}>
-							Add #{this.state.items.length + 1}
-						</button>
-					</form>
-				</label>
+			<Router>
+				<Card className="App">
+					<CardMedia>
+						<img src={imageUrl} className="photo"/>
+						<h1 className="App-title">TODO React App</h1>
+					</CardMedia>
+					<CardContent>
+						<ul>
+							<li><Link to="/todo">Todo</Link></li>
+							<li><Link to="/">Login</Link></li>
+						</ul>
 
-				<table className="table table-hover">
-					<thead>
-					<tr>
-						<th>Activity</th>
-						<th>Priority</th>
-						<th>Date</th>
-					</tr>
-					</thead>
-					<tbody>
-					{this.renderRow(this.state.items)}
-					</tbody>
-				</table>
-				<Login/>
-			</div>
+						<div>
+							{this.state.isLoggedLn ? (
+								<Route path="/todo" component={TodoAppView} />
+							) : (
+								<Route exact path="/" component={LoginView} />
+							)}
+						</div>
+					</CardContent>
+				</Card>
+			</Router>
 		);
+	}
+
+	handleLogChange() {
+		alert(this.state.isLoggedLn)
 	}
 
 
---
+++ src/Components/TodoApp.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Login} from './Login'
+
+export class TodoApp extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        const todos = [
+            {text: "Learn React", priority: 5, dueDate: new Date()},
+            {text: "Learn about APIs", priority: 4, dueDate: new Date(2020, 1, 23)},
+            {text: "write TODO App", priority: 3, dueDate: new Date(2020, 1, 30)}];
+
+        this.state = {items: todos, text: '', priority: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.renderRow = this.renderRow.bind(this);
+    }
+
+    renderRow(array) {
+        return array.map((item, i) =>
+            <tr key={i}>
+                <td>{item.text}</td>
+                <td>{item.priority}</td>
+                <td>{item.dueDate.toString()}</td>
+            </tr>
+        );
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        if (!this.state.text.length || this.state.priority == '') {
+            document.getElementById("form").reset();
+            return;
+        }
+        const newItem = {text: this.state.text, priority: this.state.priority, dueDate: new Date()};
+        this.setState(prevState => ({
+            items: prevState.items.concat(newItem)
+        }));
+        document.getElementById("form").reset();
+    }
+
+    handleChange(event) {
+        const key = event.target.name;
+        const value = event.target.value;
+        this.setState({[key]: value});
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h3>TODO</h3>
+                <label>
+                    <form id="form" onSubmit={this.handleSubmit}>
+                        <label id="activity">Activity :</label>
+                        <input type="text" name="text" onChange={this.handleChange}/>
+                        <label id="Priority">Priority :</label>
+                        <input type="text" name="priority" onChange={this.handleChange}/>
+                        <button style={{marginTop: "40px"}}>
+                            Add #{this.state.items.length + 1}
+                        </button>
+                    </form>
+                </label>
+
+                <table className="table table-hover">
+                    <thead>
+                    <tr>
+                        <th>Activity</th>
+                        <th>Priority</th>
+                        <th>Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.renderRow(this.state.items)}
+                    </tbody>
+                </table>
+                <Login/>
+            </div>
+        );
+    }
+
+
+}
